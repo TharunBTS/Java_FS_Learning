88457de9b0efa15f7a36db4c9106eedbbc780380
TharunBTS <tharunkarthick0509@gmail.com>
2025-03-03 14:22:49 +0530

    program added

--- Sample1/src/prg1.java
+++ Sample1/src/prg1.java
@@ -1,26 +1,61 @@
-import java.util.*;
+import java.util.*;    
 
-class pg1
+
+class Employee implements Comparable<Employee>
 {
-	int id;
-	String name;
-	pg1()
+	private int id;
+	private String name;
+	public Employee()
 	{
-		this .id = 12;
-		this.name = "ram";
+		
+	}
+	public Employee(int id,String name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int compareTo(Employee o) {
+		return this.id - o.id;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
 	}
 }
 
-class prg1
+class NameComparator implements Comparator<Employee>
+{
+	public int compare(Employee o1, Employee o2)
+	{
+		return o1.getName().compareTo(o2.getName());
+	}
+}
+
+public class prg1
 {
 	public static void main(String args[])
 	{
-//		Scanner ip = new Scanner(System.in);
-		pg1 p = new pg1();
-		pg1 p2 = p;
-		p.name = "kumar";
-		System.out.println(p.name);
-		System.out.println(p2.name);
+		Employee e = new Employee();
+//		Employee e = new Employee(9,"ram");
+//		Employee e = new Employee(3,"ram");
+		List<Employee> l = new ArrayList<>();
+		l.add(new Employee(1,"aam"));
+		l.add(new Employee(9,"aghu"));
+		l.add(new Employee(3,"ash"));
+		l.add(new Employee(5,"gary"));
+		System.out.println(e.getId()+" "+e.getName());
+		System.out.println(l);
+		Collections.sort(l);
+		System.out.println(l);
+		
+		Collections.sort(l,new NameComparator());
+		System.out.println(l);
 		
 	}
 }
